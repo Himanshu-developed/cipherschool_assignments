82975bb402081ef8ad9421f99421bc9d2cb975fc
himnshu2211 <71185884+himnshu2211@users.noreply.github.com>
2021-02-14 14:49:56 +0530

    Add files via upload

---
+++ Find majority/majorityElement.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+void majorityElement(int arr[], int n)
+{
+	int pos=-1,i,j, flag=0;
+	for(i=0;i<n;i++)
+	{
+	    int count=0;
+		for(j=0;j<n;j++)
+		{
+			if(arr[i]==arr[j])
+			{
+				count++;
+			}
+		
+		}
+			if (count>flag)
+			{
+				flag=count;
+				pos=i;
+		
+			}
+	
+	}
+		if(flag > n/2)
+		{
+			cout<<"Most frequently occured element is::"<<arr[pos];
+		}
+		else 
+		{
+			cout<<"No Element Found";
+		}
+}
+int main()
+{
+	int n, arr[n],i;
+	cout<<"Enter the size of array::";
+	cin>>n;
+	cout<<"Enter the element of array::";
+	for(i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	majorityElement( arr,n);
+	return 0;
+}
